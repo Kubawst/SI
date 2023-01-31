d9068a6ae0d7b83321db132b2ad3ffeb6f64fdb7
Kubawst <103138952+Kubawst@users.noreply.github.com>
2023-01-31 20:36:35 +0100

    Add files via upload

---
+++ rpart.R
@@ -0,0 +1,26 @@
+library(rpart)
+library(rattle)
+
+data = read.csv("penguins_size.csv",header = TRUE, 
+stringsAsFactors = T)
+
+idx=sample(2,344,replace = T, prob=c(0.8,0.2))
+
+train = data[idx == 1,]
+test = data[idx ==2,]
+
+model = rpart(species ~ . , data=train)
+
+fancyRpartPlot(model)
+#plot(model)
+
+p = predict(model, test, type = "class")
+length(p)
+p
+test$species
+length(data$species)
+cf = table (p, test$species)
+cf
+
+
+cat("poprawność", sum(diag(cf))/sum(cf)*100, "%")
